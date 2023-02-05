f83a26e06167fed9080b64cb88ad86ec2b83869f
guillaumeq <guillaumeq@eisti.eu>
2023-02-05 21:36:48 +0100

    Auto-generated commit [ci skip]

--- src/app/status.ts
+++
@@ -1,9 +0,0 @@
-export class Status {
-    statusList : boolean[];
-    statusGeneral: boolean;
-
-    constructor() {
-        this.statusGeneral = false;
-        this.statusList = [true, true, true, true];
-      }
-}
